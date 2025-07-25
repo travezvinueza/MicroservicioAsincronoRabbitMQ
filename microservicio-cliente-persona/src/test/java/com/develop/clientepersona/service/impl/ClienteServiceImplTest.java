0d31bbfb52328ca0fa18e0994f0d78e22658b89b
travezvinueza <travezvinueza@gmail.com>
2025-07-25 10:50:01 -0500

    Update application properties for testing environment; refactor ClienteServiceImplTest to use JUnit 5 and enhance client creation test case.

--- microservicio-cliente-persona/src/test/java/com/develop/clientepersona/service/impl/ClienteServiceImplTest.java
+++ microservicio-cliente-persona/src/test/java/com/develop/clientepersona/service/impl/ClienteServiceImplTest.java
@@ -1,18 +1,29 @@
 package com.develop.clientepersona.service.impl;
 
-import com.develop.clientepersona.controller.ClienteController;
+import com.develop.clientepersona.dto.ClienteDTO;
+import com.develop.clientepersona.enums.GenderPerson;
 import com.develop.clientepersona.repository.ClienteRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @SpringBootTest
 @AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+@TestPropertySource("classpath:application-test.properties")
 class ClienteServiceImplTest {
 
     @Autowired
@@ -22,7 +33,7 @@ class ClienteServiceImplTest {
     private ClienteRepository clienteRepository;
 
     @Autowired
-    private ClienteController clienteController;
+    private ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp() {
@@ -30,15 +41,31 @@ class ClienteServiceImplTest {
     }
 
 
-//    @Test
-//    void crearCliente() throws Exception {
-//        String clienteJson = "{\"fullName\":\"Rivera Pablo\",\"genderPerson\":\"MASCULINO\",\"age\":\"30\",  \"identification\":\"1722722343\",\"address\":\"Loja\", \"phone\":\"0978564532\", \"password\":\"12345678\"}";
-//
-//        mockMvc.perform(post("/api/v1/clientes")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(clienteJson))
-//                .andExpect(status().isCreated())
-//                .andExpect(jsonPath("$.fullName").value("Rivera Pablo"))
-//                .andExpect(jsonPath("$.identifitacion").value("1722722343"));
-//    }
+    @Test
+    void crearCliente_deberiaRetornar201YClienteCreado() throws Exception {
+
+        ClienteDTO nuevoCliente = new ClienteDTO();
+        nuevoCliente.setFullName("Rivera Pablo");
+        nuevoCliente.setGenderPerson(GenderPerson.MASCULINO);
+        nuevoCliente.setAge(30);
+        nuevoCliente.setIdentification("1722722343");
+        nuevoCliente.setAddress("Loja");
+        nuevoCliente.setPhone("0978564532");
+        nuevoCliente.setPassword("12345678");
+        nuevoCliente.setState(true);
+
+        String clienteJson = objectMapper.writeValueAsString(nuevoCliente);
+
+        mockMvc.perform(post("/api/v1/clientes")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(clienteJson))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.fullName", is("Rivera Pablo")))
+                .andExpect(jsonPath("$.identification", is("1722722343")))
+                .andExpect(jsonPath("$.genderPerson", is("MASCULINO")))
+                .andExpect(jsonPath("$.age", is(30)))
+                .andExpect(jsonPath("$.address", is("Loja")))
+                .andExpect(jsonPath("$.phone", is("0978564532")));
+    }
 }
